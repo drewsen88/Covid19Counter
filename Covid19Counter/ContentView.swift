fdc8b76dab37b892384895059e6dc47ba0effbfe
Drew Sen <drew.sen@fireflysoftware.com>
2020-03-28 19:00:28 -0700

    Initial Commit

---
+++ Covid19Counter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19Counter
+//
+//  Created by Drew Sen on 2020-03-28.
+//  Copyright © 2020 Drew Sen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covid19CounterTests/Covid19CounterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Covid19CounterTests.swift
+//  Covid19CounterTests
+//
+//  Created by Drew Sen on 2020-03-28.
+//  Copyright © 2020 Drew Sen. All rights reserved.
+//
+
+import XCTest
+@testable import Covid19Counter
+
+class Covid19CounterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
